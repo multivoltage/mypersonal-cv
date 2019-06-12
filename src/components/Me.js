dc141ae250bf8b7a9dfc437e0478178c061a2b02
Diego Tonini <diego.tonini@abstract.it>
2019-06-12 10:00:23 +0200

    upgrade cv

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,44 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Icon from './assets/svg/outline-date_range-24px.svg';
-import '../me.scss';
+import "../me.scss";
 
-const image_url = "https://media.licdn.com/dms/image/C5103AQG9OzZVZYLoxw/profile-displayphoto-shrink_200_200/0?e=1550707200&v=beta&t=s4nhHVl1Y_FsNglUuszWBhSvwgicwrmrJ0FWNFN8nVs";
+const image_url =
+  "https://media.licdn.com/dms/image/C5103AQG9OzZVZYLoxw/profile-displayphoto-shrink_200_200/0?e=1565827200&v=beta&t=JhFn0Rtd6_RMM8zDvhD6rPk2_0n0w997BrPJYtGBSyU";
 
 class Me extends Component {
-    render() {
+  render() {
+    const { phone, email, linkedin, github, location } = this.props.me;
+    return (
+      <div class="me--row">
+        <div className="colum">
+          <img className="imageProfile" src={image_url} />
+        </div>
+        <div className="colum">
+          <span className="location">
+            <i class="material-icons">location_on</i>
+            <span className="text">{location}</span>
+          </span>
+          <span className="phone">
+            <i class="material-icons">phone</i>
+            <span className="text">{phone}</span>
+          </span>
+          <span className="email">
+            <i class="material-icons">alternate_email</i>
+            <a href={`mailto:${email}`}>
+              <span mailTo={email} className="text">
+                {email}
+              </span>
+            </a>
+          </span>
+          <span className="linkedin">
+            <i className="icon-linkedin" />
+            <a href={linkedin}>
+              <span className="text">{linkedin}</span>
+            </a>
+          </span>
 
-        const { phone, email, linkedin, github, location } = this.props.me;
-        return (
-            <div class="me--row">
-                <div className="colum">
-                    <img className="imageProfile" src={image_url} />
-                </div>
-                <div className="colum">
-                    <span className="location">
-                    <i class="material-icons">location_on</i>
-                        <span className="text">{location}</span>
-                    </span>
-                    <span className="phone">
-                        <i class="material-icons">phone</i>
-                        <span className="text">{phone}</span>
-                    </span>
-                    <span className="email">
-                        <i class="material-icons">alternate_email</i>
-                        <a href={`mailto:${email}`}><span mailTo={email} className="text">{email}</span></a>
-                    </span>
-                    <span className="linkedin">
-                        <i className="icon-linkedin" />
-                        <a href={linkedin}><span className="text">{linkedin}</span></a>
-                    </span>
-
-                    <span className="github">
-                        <i className="icon-github" />
-                        <a href={github}><span className="text">{github}</span></a>
-                    </span>
-                </div>
-            </div>
-        );
-    }
+          <span className="github">
+            <i className="icon-github" />
+            <a href={github}>
+              <span className="text">{github}</span>
+            </a>
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Me;
